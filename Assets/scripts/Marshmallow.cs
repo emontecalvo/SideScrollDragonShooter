9d980378a8c36241f57bb03d1461747304bc9887
emontecalvo <emilymontecalvo@gmail.com>
2018-04-26 13:41:11 -0700

    added marshmallow

---
+++ Assets/scripts/Marshmallow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marshmallow : MonoBehaviour {
+
+	public Vector3 MarshmallowPosition;
+
+	public float LeftWall = -9.0f;
+
+
+	void Start () {
+		MarshmallowMgr.inst.Register (this);
+		MarshmallowPosition = transform.position;
+	}
+
+	void Update () {
+		MarshmallowMovement ();
+		MarshmallowPosition = transform.position;
+
+	}
+
+	void MarshmallowMovement() {
+		Vector3 myPosition = transform.position;
+
+		if (myPosition.x > LeftWall) {
+			myPosition.x -= .03f;
+		} else {
+			OnDestroy ();
+			Destroy (gameObject);
+		} 
+
+		transform.position = myPosition;
+	}
+
+	void OnDestroy() {
+		if (MarshmallowMgr.inst != null) {
+			MarshmallowMgr.inst.Unregister (this);
+		}
+	}
+}
---
+++ Assets/scripts/MarshmallowMgr.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarshmallowMgr : MonoBehaviour {
+
+	private static MarshmallowMgr _inst = null;
+
+	public static MarshmallowMgr inst {
+		get {
+			if (_inst == null) {
+				_inst = FindObjectOfType<MarshmallowMgr> ();
+			}
+			return _inst;
+		}
+	}
+
+	public List <Marshmallow> AllMarshmallows = new List<Marshmallow> ();
+
+	public void Register (Marshmallow marshmallow) {
+
+		AllMarshmallows.Add (marshmallow);
+
+	}
+
+	public void Unregister (Marshmallow marshmallow) {
+		AllMarshmallows.Remove (marshmallow);
+	}
+}
