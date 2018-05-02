e3ae09468c53ee13cf8b587461b1248064e461db
emontecalvo <emilymontecalvo@gmail.com>
2018-05-02 15:14:57 -0700

    adding sin wave to the mallows

--- Assets/scripts/Marshmallow.cs
+++ Assets/scripts/Marshmallow.cs
@@ -9,16 +9,32 @@ public class Marshmallow : MonoBehaviour {
 	public float LeftWall = -9.0f;
 
 
+
+
+
+	float originalY;
+
+	public float floatStrength = 1; // You can change this in the Unity Editor to 
+	// change the range of y positions that are possible.
+
+
 	void Start () {
 		MarshmallowMgr.inst.Register (this);
 		MarshmallowPosition = transform.position;
+
+		this.originalY = this.transform.position.y;
 	}
 
 	void Update () {
 		MarshmallowMovement ();
 		MarshmallowPosition = transform.position;
 
+		transform.position = new Vector3(transform.position.x,
+			originalY + ((float)Mathf.Sin(Time.time) * floatStrength),
+			transform.position.z);
 	}
+
+
 
 	void MarshmallowMovement() {
 		Vector3 myPosition = transform.position;
